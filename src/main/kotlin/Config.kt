62afd32ebfbb93b0e07b41a60db948fb562f438f
chuck-stein <cfgstein@gmail.com>
2021-07-08 18:33:13 -0400

    Enlarge grid

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
-const val NUM_ROWS = 30
-const val NUM_COLUMNS = 60
+const val NUM_ROWS = 50
+const val NUM_COLUMNS = 100
 const val CELL_SIZE_DP = 15
 const val CELL_BORDER_SIZE_DP = 1
 const val ITERATION_TIME_MILLIS = 200L
